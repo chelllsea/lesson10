7cf1589aacfc984d55958c593dd58fc061cba28b
chelllsea <44092576+chelllsea@users.noreply.github.com>
2019-01-16 14:38:09 -0500

    Add files via upload

---
+++ Lesson 10/boroughbot.py
@@ -0,0 +1,15 @@
+print('-' * 65)
+
+print('Best Borough Bot:')
+
+print()
+
+borough = input('What borough are you from? ')
+
+if borough == 'Brooklyn':
+	print('Nice, Brooklyn is the best borough!')
+
+else:
+	print('Too bad, Brooklyn is the best!')
+
+print('-'*65)
---
+++ Lesson 10/lifebot.py
@@ -0,0 +1,16 @@
+print('*' * 60)
+
+print('Life Decision Bot')
+
+print()
+
+answer = input('Are you happy? ')
+
+if answer == 'Yes':
+	print('Sing a happy song!')
+
+else: 
+	print("That's too bad.")
+	print("Sucks to hear that but uh feel better i guess")
+
+print('*' * 60)
